9067112fddf148ddc36c8167df9a62a164e29faf
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-14 23:45:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(9) # same as... array << 9
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
